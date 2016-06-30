db60818ff4c26b60a8f5544602baa38b5c01e457
Mohit Rawat <mohit.rawat@bizense.com>
2016-06-30 18:09:09 +0530

    adding version openrtb 2.3 in openrtb_bid_request_parser.cc

--- rtbkit/plugins/bid_request/openrtb_bid_request_parser.cc
+++ rtbkit/plugins/bid_request/openrtb_bid_request_parser.cc
@@ -32,7 +32,9 @@ openRTBBidRequestParserFactory(const std::string & version)
         return std::unique_ptr<OpenRTBBidRequestParser2point1>(new OpenRTBBidRequestParser2point1());
     } else if(version == "2.2") {
         return std::unique_ptr<OpenRTBBidRequestParser2point2>(new OpenRTBBidRequestParser2point2());
-    }
+    } else if(version == "2.3") {
+        return std::unique_ptr<OpenRTBBidRequestParser2point3>(new OpenRTBBidRequestParser2point3());
+	}
 
     THROW(OpenRTBBidRequestLogs::error) << "Version : " << version << " not supported in RTBkit." << endl;
 }
